848d9ace84216a64b3faef3276e4d0c7b04446bd
Marshal <coolmarshal27@gmail.com>
2024-11-05 15:51:43 +0530

    sup

--- script.js
+++ script.js
@@ -1,83 +1,101 @@
 const container = document.getElementById('apple');
-const container1 = document.getElementById('apple1');
-const container2 = document.getElementById('apple2');
+// const container1 = document.getElementById('apple1');
+// const container2 = document.getElementById('apple2');
 const text = document.getElementById('tax');
-const text1 = document.getElementById('tax1');
-const text2 = document.getElementById('tax2');
+// const text1 = document.getElementById('tax1');
+// const text2 = document.getElementById('tax2');
 var intofire = document.getElementById("intofire")
 var rootElement = document.documentElement
 
-const totalTime = 16000;
-const breatheTime = 4000;
-const holdTime = 2000;
-const outTime = 8000;
+const totalTime = 16500;
+const breatheTime = (totalTime / 8) * 2;
+const holdTime = totalTime / 8;
+const outTime = totalTime / 2;
 
-const totalTime1 = 16000;
-const breatheTime1 = (totalTime / 5) * 2;
-const holdTime1 = totalTime / 5;
+// const totalTime1 = 16000;
+// const breatheTime1 = (totalTime / 5) * 2;
+// const holdTime1 = totalTime / 5;
 
-const totalTime2 = 7500;
-const breatheTime2 = (totalTime / 5) * 2;
-const holdTime2 = totalTime / 5;
+// const totalTime2 = 7500;
+// const breatheTime2 = (totalTime / 5) * 2;
+// const holdTime2 = totalTime / 5;
 
+// function breath4282Animation() {
+//   text.innerText = 'Breathe In!';
+//   container.className = 'apple grow';
 
+//   setTimeout(() => {
+//     text.innerText = 'Hold';
 
-function breathAnimation() {
-  text.innerText = 'Breathe In!';
-  container.className = 'apple grow';
+//     setTimeout(() => {
+//       text.innerText = 'Breathe Out!';
+//       container.className = 'apple shrink';
 
+//       setTimeout(() => {
+//         text.innerText = 'Hold';
+        
+//     }, outTime)
+//    }, holdTime);
+//   }, breatheTime);
+// }
+
+// setInterval(breath4282Animation, totalTime);
+
+const stages = [
+  {text: "Breathe In",duration: 4000},
+  {text: "Hold",duration: 2000},
+  {text: "Breathe Out",duration: 8000},
+  {text: "Hold",duration: 2000},
+];
+let currentStage=0;
+
+function breath4282Animation() {
+  text.textContent = stages[currentStage].text;
   setTimeout(() => {
-    text.innerText = 'Hold';
+    currentStage = (currentStage + 1)%stages.length;
+    breath4282Animation();
+}, stages[currentStage].duration);
 
-    setTimeout(() => {
-      text.innerText = 'Breathe Out!';
-      container.className = 'apple shrink';
+  }
 
-      setTimeout(() => {
-        text.innerText = 'Hold';
-    } 
-    , outTime)
-   }, holdTime);
-  }, breatheTime);
-}
 
-setInterval(breathAnimation, totalTime);
 
-breathAnimation1();
 
-function breathAnimation1() {
-  text1.innerText = 'Breathe In!';
-  container1.className = 'apple1 grow';
+// breathAnimation1();
 
-  setTimeout(() => {
-    text1.innerText = 'Hold';
+// function breathAnimation1() {
+//   text1.innerText = 'Breathe In!';
+//   container1.className = 'apple1 grow';
 
-    setTimeout(() => {
-      text1.innerText = 'Breathe Out!';
-      container1.className = 'apple1 shrink';
-    }, holdTime1);
-  }, breatheTime1);
-}
+//   setTimeout(() => {
+//     text1.innerText = 'Hold';
 
-setInterval(breathAnimation1, totalTime1);
+//     setTimeout(() => {
+//       text1.innerText = 'Breathe Out!';
+//       container1.className = 'apple1 shrink';
+//     }, holdTime1);
+//   }, breatheTime1);
+// }
 
-breathAnimation2();
+// setInterval(breathAnimation1, totalTime1);
 
-function breathAnimation2() {
-  text2.innerText = 'Breathe In!';
-  container2.className = 'apple2 grow';
+// breathAnimation2();
 
-  setTimeout(() => {
-    text2.innerText = 'Hold';
+// function breathAnimation2() {
+//   text2.innerText = 'Breathe In!';
+//   container2.className = 'apple2 grow';
 
-    setTimeout(() => {
-      text2.innerText = 'Breathe Out!';
-      container2.className = 'apple2 shrink';
-    }, holdTime2);
-  }, breatheTime2);
-}
+//   setTimeout(() => {
+//     text2.innerText = 'Hold';
 
-setInterval(breathAnimation2, totalTime2);
+//     setTimeout(() => {
+//       text2.innerText = 'Breathe Out!';
+//       container2.className = 'apple2 shrink';
+//     }, holdTime2);
+//   }, breatheTime2);
+// }
+
+// setInterval(breathAnimation2, totalTime2);
 
 
 function IntoFire() {
